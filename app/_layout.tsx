e50d6c9fc7ecbc1197390c337318b447db32e74b
Diogo-Nandes <120397281+Diogo-Nandes@users.noreply.github.com>
2025-06-02 23:00:58 +0100

    Add files via upload

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -45,6 +45,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
